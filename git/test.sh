b8d3dba154adfa15b52f9005742605b7b4ab15b5
root <root@ip-172-31-36-57.ap-south-1.compute.internal>
2024-07-20 10:07:18 +0000

    update rolling

--- git/test.sh
+++ git/test.sh
@@ -1,1 +1,1 @@
-for i in {1..10000}; do curl -s -o /dev/null -w "%{http_code}\n" http://a9f61f870dbff487e8bff525db1aec69-1267525678.ap-south-1.elb.amazonaws.com/; done
+for i in {1..10000}; do curl -s -o /dev/null -w "%{http_code}\n" http://ac073296666fd4ea7b1cbe185a576b0c-522242850.ap-south-1.elb.amazonaws.com/nginx ; done
